15e501eeee38953e9542c71a88a00bbc9b5c0c01
ziltz <jwclee92@gmail.com>
2017-04-20 02:36:20 +0100

    - rig_transform remove annoying warning msgs

--- python/rig_transform.py
+++ python/rig_transform.py
@@ -27,24 +27,20 @@ class rig_transform(rig_object):
 			pm.parent(self.child, self.object)
 
 
-		offset = None
+		offset = ''
 		if pm.objExists(self.target):
 			offset = rig_transform(0).object
-			print self.object+' parent to offset target '+'\n'
-			print self.target+' target to '+ offset+'\n'
 			pm.parent(self.object, offset)
 			pm.delete(pm.parentConstraint(self.target, offset))
 
 		try :
-			print self.object +' parent to '+self.parent+'\n'
 			pm.parent(self.object, self.parent)
 		except:
-			print self.object+ ' parent to world '+'\n'
 			pm.parent(self.object, w=True)
 		finally:
-			try:
+			if pm.objExists(offset):
 				pm.delete(offset)
-			except:
+			else:
 				offset = None
 
 		super(rig_transform, self).__init__(self.object, **kwds)
@@ -53,33 +49,36 @@ class rig_transform(rig_object):
 		mc.select(cl=True)
 
 		typeDict = {
-			'group' : (mc.group(em=True, n=self.name+"_GRP"), "GRP" ) ,
-			'locator' : ( mc.spaceLocator( n=self.name+"_LOC" )[0], "LOC" ),
-			'joint': ( mc.joint(n=self.name + "_JNT"), "JNT" )
+			'group' : ( self._getTransform('_group') , "GRP" ) ,
+			'locator' : ( self._getTransform('_locator'), "LOC" ),
+			'joint': ( self._getTransform('_joint') , "JNT" )
 		}
 
-		obj = (typeDict['group'])[0]
+		obj=''
 		self.suffix = (typeDict[_type])[1]
 		try:
 			for t in typeDict:
 				if t is _type:
-					obj = (typeDict[_type])[0]
-				else:
-					try:
-						mc.delete( (typeDict[t])[0] )
-					except:
-						pm.warning(typeDict[t][0]+' does not exist')
-
+					obj = (typeDict[_type])[0]()
+					break
 		except KeyError:
+			obj = (typeDict['group'])[0]()
 			print 'No such type object in dictionary, making default group'
-			for t in typeDict:
-				if t is not 'group':
-					try:
-						mc.delete( (typeDict[t])[0] )
-					except:
-						pm.warning(typeDict[t][0]+' does not exist')
 
-		return pm.PyNode(obj)
+		return obj
+
+	def _getTransform(self, transform):
+		func = getattr(self, transform)
+		return func
+
+	def _group(self):
+		return mc.group(em=True, n=self.name+"_GRP")
+
+	def _locator (self):
+		return mc.spaceLocator( n=self.name+"_LOC" )[0]
+
+	def _joint (self):
+		return mc.joint(n=self.name + "_JNT")
 
 	def offsetGroup(self, origin=0, name=None):
 		if name is None:
